103c9061c715b1c7b9d8c9ac05ef198322633beb
José Luis Fernández Bueno <prueba@MacBook-de-Krama.local>
2018-09-22 20:01:26 +0200

    Creado proyecto. Creada arquitectura inicial

---
+++ LectorRSS/LectorRSS/Modules/NewDetail/NewDetailModule.swift
@@ -0,0 +1,9 @@
+//
+//  NewDetailModule.swift
+//  LectorRSS
+//
+//  Created by José Luis Fernández Bueno on 22/9/18.
+//  Copyright © 2018 joseluisfdezbueno. All rights reserved.
+//
+
+import Foundation
---
+++ LectorRSS/LectorRSS/Modules/NewsList/View/NewsListViewController.swift
@@ -0,0 +1,24 @@
+//
+//  NewsListViewController.swift
+//  LectorRSS
+//
+//  Created by prueba on 22/9/18.
+//  Copyright © 2018 joseluisfdezbueno. All rights reserved.
+//
+
+import UIKit
+
+class NewsListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
